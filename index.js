58861cff1fff338aa44d2fffa435d287e885bd07
shankar.psn@gmail.com <shankar.psn@gmail.com>
2023-12-23 20:25:01 +0530

    Added middleware fucntions for logging requests

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.use(express.json());
 
 let books =[{bookname:'let us c'}, {bookname:'let us c++'}];
 
+let reqLogger = (req, res, next) => {
+  console.log('new request');
+  next();
+};
+
+app.use(reqLogger);
+
 //get all books
 app.get('/api/books', (req, res) => {
   res.json(books);
@@ -25,6 +32,21 @@ app.post('/api/books', (req, res) => {
 app.delete('/api/books', (req, res) => {
   books.pop();
   res.send('book removed');
+});
+
+app.delete('/api/books/:id', (req, res) => {
+  try {
+    let arrayid = req.params.id;
+    delete books[arrayid];
+    res.send('book removed');
+  } catch (error) {
+    console.log('error');
+  }
+
+});
+
+app.use(function(err, req, res, next) {
+  res.end('error');
 });
 
 app.listen(3000, () => {console.log('server is running ...')});
